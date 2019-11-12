4b6027f6854526300171948bff0a8567b8bb4dd5
Christoph Coijanovic <mail@coijanovic.com>
2019-11-12 21:51:49 +0100

    first tests

--- startpage.py
+++ startpage.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     r = "Hi Christoph"
-    return r
+    return render_template('index.html');
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=True,host=config['hostip'],port=config['hostport'])
