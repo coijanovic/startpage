b13ebf561c416386b60a4fec6568bc7bc864bda4
Christoph Coijanovic <mail@coijanovic.com>
2019-11-13 09:14:27 +0100

    commentts

--- startpage.py
+++ startpage.py
@@ -4,6 +4,7 @@ import yaml
 import json
 import wget
 import time
+import datetime
 
 if os.path.exists('config.yaml'):
     configfile = 'config.yaml'
@@ -20,6 +21,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # 1. Greeting
+
+    # 2. Date
+
+    # 3. Weather
     url = "https://api.openweathermap.org/data/2.5/weather?q=Karlsruhe,de&id=4737316&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece"
     f = wget.download(url, out="weather.json")
     weather = json.loads(open(f).read())
