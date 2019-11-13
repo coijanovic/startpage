61b8bc483142cdcb2008b728927928f136867ba5
Christoph Coijanovic <mail@coijanovic.com>
2019-11-13 08:47:49 +0100

    added weather

--- startpage.py
+++ startpage.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os.path
 import yaml
+import json
+import wget
 import time
 
 if os.path.exists('config.yaml'):
@@ -18,8 +20,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    url = "https://api.openweathermap.org/data/2.5/weather?q=Karlsruhe,de&id=4737316&units=metric&appid=e5b292ae2f9dae5f29e11499c2d82ece"
+    f = wget.download(url, out="weather.json")
+    weather = json.loads(open(f).read())
+    w = "Outside, it's {t} °C and {h}".format(t=round(weather['main']['temp']), h=weather['weather'][0]['description'])
+    return render_template('index.html', w=w);
     r = "Hi Christoph"
-    return render_template('index.html');
 
 if __name__ == '__main__':
     app.run(debug=True,host=config['hostip'],port=config['hostport'])
